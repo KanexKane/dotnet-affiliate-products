9c3bf771eaf0bf588b08838750ab2f9a3d32c819
KanexKane <kanexkane@gmail.com>
2025-03-16 02:50:30 +0700

    ปรับการแสดงผลให้สวยขึ้นอีกนิด

--- AffiliateProduct/Models/Product.cs
+++ AffiliateProduct/Models/Product.cs
@@ -7,9 +7,9 @@ public class Product
 {
     public string? Name { get; set; }
     public string? Image { get; set; }
-    public int Order { get; set; }
     [JsonPropertyName("shopee_url")]
     public string? ShopeeUrl { get; set; }
     [JsonPropertyName("tiktok_url")]
     public string? TiktokUrl { get; set; }
+    public int Order { get; set; }
 }
--- AffiliateProduct/Pages/Home.razor.cs
+++ AffiliateProduct/Pages/Home.razor.cs
@@ -11,8 +11,35 @@ public partial class Home
     public HttpClient HttpClient { get; set; } = default!;
 
     List<Product> products = new List<Product>();
+
+    private string selectedOrderBy = "name"; // ค่าเริ่มต้น
+
     protected override async Task OnInitializedAsync()
     {
         products = await HttpClient.GetFromJsonAsync<List<Product>>("data/product.json") ?? new List<Product>();
+
+        SortProducts();
+
+    }
+
+    private void OnSortChanged(ChangeEventArgs e)
+    {
+        selectedOrderBy = e.Value?.ToString() ?? "name"; // อัปเดตค่าที่เลือก
+        SortProducts(); // เรียงลำดับใหม่
+    }
+
+    private void SortProducts()
+    {
+
+        if (selectedOrderBy == "order")
+        {
+            products = products.OrderBy(p => p.Order).ToList();
+        }
+        else if (selectedOrderBy == "name")
+        {
+            products = products.OrderBy(p => p.Name).ToList();
+        }
+
+        StateHasChanged();
     }
 }
